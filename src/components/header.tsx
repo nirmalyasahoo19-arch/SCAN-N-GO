3cecd3c205f8853adb10e236546e4b537fdf86c6
nirmalyasahoo19-arch <nirmalyasahoo19@gmail.com>
2025-09-29 16:01:11 +0530

    Create header.tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,109 @@
+import Link from "next/link";
+import { History, LifeBuoy, Menu, User } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Logo } from "./logo";
+
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <Link
+          href="/dashboard"
+          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <Logo className="h-12 w-32" />
+          <span className="sr-only">SCAN-N-GO</span>
+        </Link>
+        <Link
+          href="/dashboard"
+          className="text-foreground transition-colors hover:text-foreground/80 font-medium"
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/dashboard/history"
+          className="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          History
+        </Link>
+        <Link
+          href="/dashboard/support"
+          className="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Contact Support
+        </Link>
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="shrink-0 md:hidden"
+          >
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Logo className="h-12 w-32" />
+              <span className="sr-only">SCAN-N-GO</span>
+            </Link>
+            <Link href="/dashboard" className="hover:text-foreground">
+              Dashboard
+            </Link>
+            <Link
+              href="/dashboard/history"
+              className="text-muted-foreground hover:text-foreground"
+            >
+              History
+            </Link>
+            <Link
+              href="/dashboard/support"
+              className="text-muted-foreground hover:text-foreground"
+            >
+              Contact Support
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <User className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href="/dashboard/support">Support</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link href="/">Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+}
