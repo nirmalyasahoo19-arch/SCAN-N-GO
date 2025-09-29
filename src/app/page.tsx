1c7bac15bbd433a13f756bc3215174ad1d58b154
nirmalyasahoo19-arch <nirmalyasahoo19@gmail.com>
2025-09-29 16:11:45 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '@/components/auth/login-form';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-4">
+      <LoginForm />
+    </main>
+  );
+}
